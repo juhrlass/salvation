1395738a63a49191da9dfe3253a0e958d48b6707
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-04 23:56:48 +0100

    Updates

--- src/components/BingoGame.tsx
+++ src/components/BingoGame.tsx
@@ -1,54 +1,14 @@
-import { useEffect, useState } from "react";
-import { BingoComponent } from "salvation"
+import { useCallback, useState } from "react";
+import { BingoComponent } from "salvation";
+import {loadConfettiPreset} from "tsparticles-preset-confetti";
+import Particles from "react-particles";
 
-interface BingoCounterProps {
-  result:number | null;
-  maxNumber:number;
-  durationInMs:number;
-}
 
-export const BingoCounter = (props: BingoCounterProps) => {
-  // number to increment to
-  // duration of count in seconds
 
-  const number = props.result
-  const durationInMs = props.durationInMs
 
-  // number displayed by component
-  const [count, setCount] = useState(0)
 
-  function getRandomInt(max:number) {
-    return Math.floor(Math.random() * max)
-  }
-
-  useEffect(() => {
-    let start = 0
-    const end = 200
-
-    const incrementTime = (durationInMs / end)
-
-    // timer increments start counter
-    // then updates count
-    // ends if start reaches end
-    if(number!=null) {
-      const timer = setInterval(() => {
-        start += 1
-        setCount(getRandomInt(props.maxNumber))
-        if (start === end) {
-          setCount(number)
-          clearInterval(timer)
-        }
-      }, incrementTime)
-    }
-
-    // dependency array
-  }, [number, durationInMs])
-
-  return (
-      <p className="text-[10em] text-center font-bold">
-        {count ? count : "-"}
-      </p>
-  )
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max)
 }
 
 interface BingoGameProps {
@@ -60,6 +20,11 @@ export const BingoGame = (props: BingoGameProps) => {
 
   const [drawnNumbers, setDrawnNumbers] = useState<number[]>([])
   const [currentNumber, setCurrentNumber] = useState<number | null>(null)
+  const [isConfetti, setIsConfetti] = useState(false);
+
+
+  const audio = new Audio("/content/drumroll.mp3");
+  audio.preload="auto"
 
   const allNumbers = Array.from(
     { length: totalNumbers },
@@ -69,15 +34,68 @@ export const BingoGame = (props: BingoGameProps) => {
     (number) => !drawnNumbers.includes(number)
   )
 
+
+
+  const particlesInit = useCallback(async (engine) => {
+    await loadConfettiPreset(engine);
+  }, [])
+
+  const particlesConfig={
+    preset: "confetti",
+    emitters: {
+      "startCount": 100,
+      "position": {
+        "x": 50,
+        "y": 15
+      },
+      "size": {
+        "width": 0,
+        "height": 0
+      },
+      "rate": {
+        "delay": 0,
+        "quantity": 0
+      },
+      "life": {
+        "duration": 0.1,
+        "count": 1
+      }
+    },
+  }
+
   const drawNextNumber = () => {
     const remainingCount = remainingNumbers.length
-
+    setIsConfetti(false)
+    audio.pause()
+    audio.currentTime=0;
     if (remainingCount > 0) {
+      audio.play()
       const randomIndex = Math.floor(Math.random() * remainingCount)
       const newNumber = remainingNumbers[randomIndex]
 
-      setCurrentNumber(newNumber)
-      setDrawnNumbers([...drawnNumbers, newNumber])
+      let start = 0
+      const end = 200
+
+      const durationInMs = 1750
+
+      const incrementTime = durationInMs / end
+
+      // timer increments start counter
+      // then updates count
+      // ends if start reaches end
+
+      const timer = setInterval(() => {
+        start += 1
+        setCurrentNumber(getRandomInt(totalNumbers))
+        if (start === end) {
+          setCurrentNumber(newNumber)
+          setDrawnNumbers([...drawnNumbers, newNumber])
+          setIsConfetti(true)
+          clearInterval(timer)
+        }
+      }, incrementTime)
+
+      // setCurrentNumber(newNumber)
     } else {
       // Hier kannst du eine Meldung anzeigen, dass alle Zahlen gezogen wurden.
       console.log("Alle Zahlen wurden gezogen!")
@@ -92,24 +110,25 @@ export const BingoGame = (props: BingoGameProps) => {
   return (
     <div className="w-full mx-auto p-4 flex flex-col grow items-center gap-y-4">
       <div className="flex w-full justify-between">
-        <div className="justify-center flex flex-col">
+        <div className="justify-start flex flex-col">
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 w-32 aspect-square"
+            className="bg-red-500 text-white text-xl font-bold px-4 py-2 rounded-full hover:bg-red-600 w-32 aspect-square"
             onClick={resetGame}
           >
             Neues Spiel
           </button>
         </div>
         <div className="flex flex-col justify-start">
-          <h1 className="text-3xl font-semibold text-center ">Bingo</h1>
+          <p className={"text-[12em] text-white"}>
+            {currentNumber ? currentNumber : "-"}
+          </p>
 
-
-          <BingoCounter result={currentNumber} maxNumber={totalNumbers} durationInMs={2000} />
+          {isConfetti &&<Particles options={particlesConfig} init={particlesInit} />}
         </div>
-        <div className="justify-center flex flex-col">
+        <div className="justify-start flex flex-col">
           {" "}
           <button
-            className="mb-4 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-32 aspect-square"
+            className="bg-green-500 text-white text-xl font-bold px-4 py-2 rounded-full hover:bg-green-600 w-32 aspect-square"
             onClick={drawNextNumber}
           >
             Nächste Zahl
