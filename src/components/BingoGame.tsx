a5507499daf8d2722b93abcc0bdcd87a3e7b9167
Joachim Uhrlaß <joachim.uhrlass@ecube.de>
2023-12-05 10:57:55 +0100

    Updates

--- src/components/BingoGame.tsx
+++ src/components/BingoGame.tsx
@@ -2,7 +2,11 @@ import { useCallback, useState } from "react";
 import Particles from "react-particles";
 import { BingoComponent } from "salvation";
 import { Engine } from "tsparticles-engine"
+import { tsParticles } from "tsparticles-engine";
+
 import { loadConfettiPreset } from "tsparticles-preset-confetti"
+import { loadStarShape } from "tsparticles-shape-star"
+
 
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max)
@@ -32,13 +36,119 @@ export const BingoGame = (props: BingoGameProps) => {
   )
 
   const particlesInit = useCallback(async (engine: Engine) => {
+    await loadStarShape(tsParticles)
     await loadConfettiPreset(engine)
   }, [])
 
   const particlesConfig = {
-    preset: "confetti",
+    fullScreen: {
+      enable: true,
+      zIndex: 100,
+    },
+    fpsLimit: 120,
+    particles: {
+      number: {
+        value: 0,
+      },
+      color: {
+        value: ["#26ccff", "#a25afd", "#ff5e7e", "#88ff5a", "#fcff42", "#ffa62d", "#ff36ff"],
+      },
+      shape: {
+        type: ["square", "circle","star"],
+      },
+      opacity: {
+        value: { min: 0, max: 1 },
+        animation: {
+          enable: true,
+          speed: 0.5,
+          startValue: "max",
+          destroy: "min",
+        },
+      },
+      size: {
+        value: 5,
+      },
+      links: {
+        enable: false,
+      },
+      life: {
+        duration: {
+          sync: true,
+          value: 20 / 6,
+        },
+        count: 1,
+      },
+      move: {
+        angle: {
+          value: 45,
+          offset: 0,
+        },
+        drift: 0,
+        enable: true,
+        gravity: {
+          enable: true,
+          acceleration: 9.81,
+        },
+        speed: 45,
+        decay: 0.1,
+        direction: -90,
+        random: true,
+        straight: false,
+        outModes: {
+          default: "none",
+          bottom: "destroy",
+        },
+      },
+      rotate: {
+        value: {
+          min: 0,
+          max: 360,
+        },
+        direction: "random",
+        animation: {
+          enable: true,
+          speed: 60,
+        },
+      },
+      tilt: {
+        direction: "random",
+        enable: true,
+        value: {
+          min: 0,
+          max: 360,
+        },
+        animation: {
+          enable: true,
+          speed: 60,
+        },
+      },
+      roll: {
+        darken: {
+          enable: true,
+          value: 25,
+        },
+        enable: true,
+        speed: {
+          min: 15,
+          max: 25,
+        },
+      },
+      wobble: {
+        distance: 60,
+        enable: true,
+        speed: {
+          min: -25,
+          max: 25,
+        },
+      },
+    },
+    detectRetina: true,
+    motion: {
+      disable: true,
+    },
     emitters: {
-      startCount: 100,
+      name: "confetti",
+      startCount: 50,
       position: {
         x: 50,
         y: 15,
@@ -56,7 +166,7 @@ export const BingoGame = (props: BingoGameProps) => {
         count: 1,
       },
     },
-  }
+  };
 
   const drawNextNumber = () => {
     const remainingCount = remainingNumbers.length
